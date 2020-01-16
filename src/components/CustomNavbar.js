1e08b403731c2fc56c5698f6c0eb882c327bd83c
JustinJoco <justin.joco@gmail.com>
2020-01-16 00:19:40 -0800

    added home nav

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -9,11 +9,13 @@ const CustomNavbar = (props) =>{
 	return (
 		<Navbar bg="secondary" expand="sm" sticky="top" variant="dark">
 		  <Navbar.Brand href="#"><img src={logo} alt="Logo"/></Navbar.Brand>
-		  <Navbar.Toggle aria-controls="basic-navbar-nav"/>
 			<Navbar.Collapse>
-				<Nav className="mr-auto" activeKey="/home">
+				<Nav className="mr-auto">
 					</Nav>
 					<Nav>
+					<Nav.Item>
+						<Nav.Link href="/#"><B>Home</B></Nav.Link>
+					</Nav.Item>
 					<Nav.Item>
 						<Nav.Link href="#about"><B>About</B></Nav.Link>
 					</Nav.Item>
