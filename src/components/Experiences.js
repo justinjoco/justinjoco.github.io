878c9657a83d2c2df448a1578a7ae2c7a468a374
JustinJoco <justin.joco@gmail.com>
2020-01-31 22:28:06 -0800

    editing fixes

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -36,7 +36,7 @@ const Experiences= (props) => {
                     <h3 className="vertical-timeline-element-title">T-Mobile US, Inc.</h3>
                     <h5 className="vertical-timeline-element-subtitle">Software Engineer</h5>
                     <h6 className="vertical-timeline-element-subtitle">Bellevue, WA</h6>
-                    <p> Developing and testing the the desktop client of a cloud-based, voice and messaging app for business customers</p>
+                    <p> Developing and testing the desktop client of a cloud-based, voice and messaging app for work productivity</p>
                     <p> Typescript(TS) | Node.js | React.js |  Redux | Electron | Jest | Git (Bitbucket) </p>
 			    	
                 </VerticalTimelineElement>
