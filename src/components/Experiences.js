1dfeeb408636b2f9aa47d4ce124c28cfc40fcb1e
Justin Joco <justin.joco@gmail.com>
2021-10-27 20:43:04 -0700

    removed some extra stuff for tmo

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -36,7 +36,7 @@ const Experiences = () => {
                     <h3 className="vertical-timeline-element-title">Lessen, Inc.</h3>
                     <h5 className="vertical-timeline-element-subtitle">Software Engineer III</h5>
                     <h6 className="vertical-timeline-element-subtitle">Seattle, WA</h6>
-                    <p> Work on back-end services of high-growth PropTech startup </p>
+                    <p> Work on back-end services of a high-growth PropTech startup </p>
                     <hr color="white"/>
                     <p> Typescript | Node.js | Express.js | Hasura | GraphQL  | Kafka |  Docker | Kubernetes | AWS | CI/CD | Git (Github) </p>
                 </VerticalTimelineElement>
@@ -51,7 +51,6 @@ const Experiences = () => {
                     <h5 className="vertical-timeline-element-subtitle">Software Engineer</h5>
                     <h6 className="vertical-timeline-element-subtitle">Bellevue, WA</h6>
                     <p> Led development, monitoring, and deployment of Visual Voicemail's (VVM) new distributed microservices to scale to handle 25M active users at 600 TPS, featuring FFmpeg audio transcoding and enforcing 90% API handler test coverage via unit and integration testing </p>
-                    <p> Designed REST APIs using Open API 2/3 spec, automated CICD with Jenkins, reported server and client-side metrics using TIG stack, uploaded server logs to Splunk for adhoc debugging and to Kafka for long-term retention, and monitored availability with AppDynamics </p>
                     <p> Developed the new VVM Android client, handling API integration and implementing features such as push notifications, accessibility, metrics reporting, and app activation</p>
                     <p> Developed new Consumer Digits desktop client, improving messaging and group call functionality </p>
                     <hr color="gray"/>
