4b275f94e29cfe81dcb2b8b83747dce3575787fe
Justin Joco <justin.joco@gmail.com>
2021-12-18 01:06:16 -0800

    cleaned up wording of lessen exp

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -36,9 +36,9 @@ const Experiences = () => {
                     <h3 className="vertical-timeline-element-title">Lessen, Inc.</h3>
                     <h5 className="vertical-timeline-element-subtitle">Software Engineer III</h5>
                     <h6 className="vertical-timeline-element-subtitle">Seattle, WA</h6>
-                    <p> Design, develop, monitor, and deploy back-end services of a rapidly growing PropTech startup, with emphases on scalability, security, and resiliency </p>
+                    <p> Design, develop, monitor, and deploy microservices plus back-end infrastructure of a rapidly growing PropTech startup, with emphases on scalability, security, and resiliency </p>
                     <hr color="white"/>
-                    <p> Typescript | Node.js | GraphQL | Docker | Kubernetes | AWS | CI/CD | Java | Spring Boot | Git (Github) </p>
+                    <p> Docker | Kubernetes | Helm | AWS | GraphQL | Typescript | Java | PostgreSQL </p>
                 </VerticalTimelineElement>
 
                 <VerticalTimelineElement
